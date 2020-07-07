677a1e94b1046813afbb7fe1a86a2dd58406edcc
Diego Raffa <diego170975@gmail.com>
2020-07-07 10:58:42 +0100

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,30 @@
-var people = [
-  {
-    usermail: "koosvandermerwe@space48.com",
-    userpassword: "codify",
-  },
-  {
-    usermail: "koosvandermerwe@space48.com",
-    userpassword: "academy",
-  },
-  {
-    usermail: "koosvandermerwe@space48.com",
-    userpassword: "forever",
-  },
-];
+// var people = [
+//   {
+//     usermail: "koosvandermerwe@space48.com",
+//     userpassword: "codify",
+//   },
+//   {
+//     usermail: "koosvandermerwe@space48.com",
+//     userpassword: "academy",
+//   },
+//   {
+//     usermail: "koosvandermerwe@space48.com",
+//     userpassword: "forever",
+//   },
+// ];
 
-function getInfo() {
-  var usermail = document.getElementById("usermail").value;
-  var userpassword = document.getElementById("userpassword").value;
-  console.log(
-    `Your usermail is ${usermail} and your password is ${userpassword}`
-  );
-}
+// function getInfo() {
+//   var usermail = document.getElementById("usermail").value;
+//   var userpassword = document.getElementById("userpassword").value;
+//   console.log(
+//     `Your usermail is ${usermail} and your password is ${userpassword}`
+//   );
+// }
+
+const myHeading = document.getElementById("myHeading");
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", () => {
+  myHeading.style.color = "red";
+});
+container.style.backgroundColor = "red";
